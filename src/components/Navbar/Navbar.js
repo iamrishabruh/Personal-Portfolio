23245fe23a671f35c790651393bb909721231c8d
Rishabh Chouhan <rishabruh@mac.lan>
2025-04-09 13:39:28 -0400

    - Update navbar with professional dark blue theme and improved mobile responsiveness - Increase video demo button size and visibility - Improve project card layout with better spacing and hover effects - Add consistent color scheme using professional blues - Fix project heading visibility issues - Optimize mobile view for better user experience

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,101 +1,38 @@
-import { useContext, useState } from 'react';
-import Brightness2Icon from '@material-ui/icons/Brightness2';
-import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded';
-import MenuIcon from '@material-ui/icons/Menu';
-import CloseIcon from '@material-ui/icons/Close';
-import { ThemeContext } from '../../theme';
-import { projects, skills, contact, leaderships } from '../../portfolio';
-import './Navbar.css';
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
-  const [{ themeName, toggleTheme }] = useContext(ThemeContext);
-  const [showNavList, setShowNavList] = useState(false);
-
-  const toggleNavList = () => setShowNavList(!showNavList);
-
-  const scrollByPixels = (pixels) => {
-    window.scrollBy({
-      top: pixels,
-      behavior: 'smooth',
-    });
-    setShowNavList(false); // Close nav list
-  };
-
   return (
-    <nav className='center nav'>
-      <button
-        type='button'
-        onClick={toggleNavList}
-        className='btn btn--icon nav__hamburger'
-        aria-label='toggle navigation'
-      >
-        {showNavList ? <CloseIcon /> : <MenuIcon />}
-      </button>
-
-      <ul className={`nav__list ${showNavList ? 'nav__list--show' : ''}`}>
-        {projects.length > 0 && (
-          <li className='nav__list-item'>
-            <button
-              type='button'
-              onClick={() => scrollByPixels(1300)}
-              className='link link--nav'
-              onKeyPress={(e) => { if (e.key === 'Enter') scrollByPixels(300); }}
-            >
-              Projects
-            </button>
-          </li>
-        )}
-
-        {leaderships.length > 0 && (
-          <li className='nav__list-item'>
-            <button
-              type='button'
-              onClick={() => scrollByPixels(2680)}
-              className='link link--nav'
-              onKeyPress={(e) => { if (e.key === 'Enter') scrollByPixels(600); }}
-            >
-              Leadership
-            </button>
-          </li>
-        )}
-
-        {skills.length > 0 && (
-          <li className='nav__list-item'>
-            <button
-              type='button'
-              onClick={() => scrollByPixels(4025)}
-              className='link link--nav'
-              onKeyPress={(e) => { if (e.key === 'Enter') scrollByPixels(900); }}
-            >
-              Skills
-            </button>
-          </li>
-        )}
-
-        {contact.email || contact.phoneNumber ? (
-          <li className='nav__list-item'>
-            <button
-              type='button'
-              onClick={() => scrollByPixels(10000)}
-              className='link link--nav'
-              onKeyPress={(e) => { if (e.key === 'Enter') scrollByPixels(1200); }}
-            >
-              Contact
-            </button>
-          </li>
-        ) : null}
+    <nav className='navbar'>
+      <ul className='nav-links'>
+        <li>
+          <Link to='/' className='nav-link'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/about' className='nav-link'>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='/projects' className='nav-link'>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to='/skills' className='nav-link'>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='/contact' className='nav-link'>
+            Contact
+          </Link>
+        </li>
       </ul>
-
-      <button
-        type='button'
-        onClick={toggleTheme}
-        className='btn btn--icon nav__theme'
-        aria-label='toggle theme'
-      >
-        {themeName === 'dark' ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
-      </button>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
