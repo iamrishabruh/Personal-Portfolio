c68dbd506478a3b55fbd27c038a6819a7cf5fd7a
Rishabh Chouhan <rishabruh@mac.lan>
2025-04-09 13:45:39 -0400

    - Update Navbar component to use concise arrow function syntax - Remove unnecessary return statement and curly braces - Maintain consistent code style with semicolons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,36 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
-const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <ul className='nav-links'>
-        <li>
-          <Link to='/' className='nav-link'>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to='/about' className='nav-link'>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to='/projects' className='nav-link'>
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link to='/skills' className='nav-link'>
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link to='/contact' className='nav-link'>
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  )
-}
+const Navbar = () => (
+  <nav className='navbar'>
+    <ul className='nav-links'>
+      <li>
+        <Link to='/' className='nav-link'>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to='/about' className='nav-link'>
+          About
+        </Link>
+      </li>
+      <li>
+        <Link to='/projects' className='nav-link'>
+          Projects
+        </Link>
+      </li>
+      <li>
+        <Link to='/skills' className='nav-link'>
+          Skills
+        </Link>
+      </li>
+      <li>
+        <Link to='/contact' className='nav-link'>
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
 
 export default Navbar
