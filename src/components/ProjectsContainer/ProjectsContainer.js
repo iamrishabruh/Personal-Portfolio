eb90d9bfd69d8c3e990500253a79f8ba52a02108
Rishabh Chouhan <rishabruh@mac.lan>
2025-04-10 00:04:06 -0400

    These changes should resolve all the ESLint errors and allow the build to proceed successfully.

--- src/components/ProjectsContainer/ProjectsContainer.js
+++ src/components/ProjectsContainer/ProjectsContainer.js
@@ -1,69 +1,75 @@
 import React from 'react';
 import uniqid from 'uniqid';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LaunchIcon from '@material-ui/icons/Launch';
 import './ProjectsContainer.css';
 
 const ProjectsContainer = ({ project }) => (
   <div className='project'>
-    <div className='project__meta'>
-      <div className='project__title'>{project.name}</div>
-      <div className='project__description'>{project.description}</div>
-      <div className='project__stack'>
+    <h3 className='project__title'>{project.name}</h3>
+    <p className='project__description'>{project.description}</p>
+    
+    {project.stack && (
+      <ul className='project__stack'>
         {project.stack.map((item) => (
-          <span key={uniqid()} className='project__stack-item'>
+          <li key={uniqid()} className='project__stack-item'>
             {item}
-          </span>
+          </li>
         ))}
-      </div>
-      <div className='project__links'>
-        {project.sourceCode && (
-          <a
-            href={project.sourceCode}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='link link--icon'
-            aria-label='View source code on GitHub'
-          >
-            <i className='fab fa-github' />
-          </a>
-        )}
-        {project.livePreview && (
-          <a
-            href={project.livePreview}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='link link--icon'
-            aria-label='View live preview'
-          >
-            <i className='fas fa-external-link-alt' />
-          </a>
-        )}
-        {project.videoDemo && (
-          <a
-            href={project.videoDemo}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='link link--icon'
-            aria-label='View video demo'
-          >
-            <i className='fas fa-video' />
-          </a>
-        )}
-      </div>
-      <div className='project__stats'>
-        {project.stars && (
-          <span className='project__stat' aria-label={`${project.stars} stars`}>
-            <i className='fas fa-star' />
-            {project.stars}
-          </span>
-        )}
-        {project.forks && (
-          <span className='project__stat' aria-label={`${project.forks} forks`}>
-            <i className='fas fa-code-branch' />
-            {project.forks}
-          </span>
-        )}
-      </div>
+      </ul>
+    )}
+
+    <div className='project__links'>
+      {project.sourceCode && (
+        <a
+          href={project.sourceCode}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='link link--icon'
+          aria-label='View source code on GitHub'
+        >
+          <GitHubIcon />
+        </a>
+      )}
+      {project.livePreview && (
+        <a
+          href={project.livePreview}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='link link--icon'
+          aria-label='View live preview'
+        >
+          <LaunchIcon />
+        </a>
+      )}
+      {project.videoDemo && (
+        <a
+          href={project.videoDemo}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='link link--icon'
+          aria-label='View video demo'
+        >
+          <i className='fas fa-video' />
+        </a>
+      )}
     </div>
+
+    <div className='project__stats'>
+      {project.stars && (
+        <span className='project__stat' aria-label={`${project.stars} stars`}>
+          <i className='fas fa-star' />
+          {project.stars}
+        </span>
+      )}
+      {project.forks && (
+        <span className='project__stat' aria-label={`${project.forks} forks`}>
+          <i className='fas fa-code-branch' />
+          {project.forks}
+        </span>
+      )}
+    </div>
+
     {project.media && (
       <div className='project__media'>
         {project.media.map((media) => (
